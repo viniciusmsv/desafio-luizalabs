dcf5332f0a5b86b54cbe646f4d65db37618b79b9
viniciusmsv <Qwaszx123!@#>
2019-08-16 03:26:19 -0300

    postman

--- docker/mongodb/load_dataset.sh
+++ docker/mongodb/load_dataset.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-cp dataset_products.csv /mnt/WDC-1TB/Desenv/Banco/mongodb/
-docker exec -it mongodb_mongo_1 mongoimport -d luizalabs -c products --type CSV --file /data/db/dataset_products.csv --headerline --authenticationDatabase admin -u root  -p root -h localhost:27017
-rm /mnt/WDC-1TB/Desenv/Banco/mongodb/dataset_products.csv
+
+PATH=/tmp/mongodb/
+
+/bin/mkdir $PATH
+
+/bin/cp ./dataset_products.csv $PATH
+
+/usr/bin/docker exec -it mongodb_mongo_1 mongoimport -d luizalabs -c products --type CSV --file /data/db/dataset_products.csv --headerline --authenticationDatabase admin -u root  -p root -h localhost:27017
+
+/bin/rm $PATH/dataset_products.csv
